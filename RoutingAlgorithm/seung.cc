407a0146dea76474796b0671b43f3c1e9c067706
csg1718 <csg1718@gmail.com>
2022-05-21 12:59:15 +0000

    route : [0,1] [1,2] [2,3] [3,5] [5,6]

--- RoutingAlgorithm/seung.cc
+++ RoutingAlgorithm/seung.cc
@@ -35,39 +35,41 @@ int main (int argc, char *argv[])
   CommandLine cmd;
   cmd.Parse (argc, argv);
 
-  std::ifstream fin("/home/junyounghoon/ns-allinone-3.29/ns-3.29/scratch/videoStreamer/input.txt");
+  std::ifstream fin("./scratch/videoStreamer/input.txt");
   std::string line;
   std::vector<std::vector<int>> route = {};
 
   route.push_back({0, 1});
 
   getline(fin, line);
-  const uint32_t nodeNum = line[0] - '0';
-  const uint32_t bridgeNum = line[2] - '0';
-
-  while (!fin.eof()) {
+  uint32_t nodeNum = line[0] - '0';
+  uint32_t bridgeNum = line[2] - '0';
+  uint32_t count = bridgeNum;
+  while (count>0) {
     getline(fin, line);
 
     std::vector<int> v_temp = {};
 
-    const uint32_t temp1 = line[0] - '0';
-    const uint32_t temp2 = line[2] - '0';
-
+    uint32_t temp1 = line[0] - '0';
+    uint32_t temp2 = line[2] - '0';
+    temp1 += 1;
+    temp2 += 1;
     v_temp.push_back(temp1);
     v_temp.push_back(temp2);
 
     route.push_back(v_temp);
-    
-    if (fin.eof()) {
+    count -= 1;
+    if (count==0) {
       std::vector<int> v_temp = {};
 
-      const uint32_t temp1 = nodeNum;
+      const uint32_t temp1 = temp2;
       const uint32_t temp2 = nodeNum + 1;
 
       v_temp.push_back(temp1);
       v_temp.push_back(temp2);
 
       route.push_back(v_temp);
+
     }
   }
 
@@ -75,418 +77,71 @@ int main (int argc, char *argv[])
   LogComponentEnable ("VideoStreamClientApplication", LOG_LEVEL_INFO);
   LogComponentEnable ("VideoStreamServerApplication", LOG_LEVEL_INFO);
 
-  if (CASE == 1)
-  {
-    NodeContainer nodes;
-    nodes.Create (2);
-
-    PointToPointHelper pointToPoint;
-    pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("100Mbps"));
-    pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
-
-    NetDeviceContainer devices;
-    devices = pointToPoint.Install (nodes);
-
-    InternetStackHelper stack;
-    stack.Install (nodes);
-
-    Ipv4AddressHelper address;
-    address.SetBase ("10.1.1.0", "255.255.255.0");
-
-    Ipv4InterfaceContainer interfaces = address.Assign (devices);
-
-    VideoStreamClientHelper videoClient (interfaces.GetAddress (0), 5000);
-    ApplicationContainer clientApp = videoClient.Install (nodes.Get (1));
-    clientApp.Start (Seconds (0.5));
-    clientApp.Stop (Seconds (100.0));
-
-    VideoStreamServerHelper videoServer (5000);
-    videoServer.SetAttribute ("MaxPacketSize", UintegerValue (1400));
-    videoServer.SetAttribute ("FrameFile", StringValue ("./scratch/videoStreamer/frameList.txt"));
-    // videoServer.SetAttribute ("FrameSize", UintegerValue (4096));
-
-    ApplicationContainer serverApp = videoServer.Install (nodes.Get (0));
-    serverApp.Start (Seconds (0.0));
-    serverApp.Stop (Seconds (100.0));
-
-    pointToPoint.EnablePcap ("videoStream", devices.Get (1), false);
-    Simulator::Run ();
-    Simulator::Destroy ();
-  }
-  else if (CASE == 2)
-  {
-    NodeContainer nodes;
-    nodes.Create (3);
-    NodeContainer n0n1= NodeContainer (nodes.Get(0), nodes.Get(1));
-    NodeContainer n0n2= NodeContainer (nodes.Get(0), nodes.Get(2));
-
-    PointToPointHelper pointToPoint;
-    pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("2Mbps"));
-    pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
-
-    NetDeviceContainer d0d1= pointToPoint.Install (n0n1);
-    NetDeviceContainer d0d2= pointToPoint.Install (n0n2);
-
-    InternetStackHelper stack;
-    stack.Install (nodes);
-
-    Ipv4AddressHelper address;
-    address.SetBase ("10.1.1.0", "255.255.255.0");
-    address.Assign (d0d1);
-
-    address.SetBase ("10.1.2.0", "255.255.255.0");
-    address.Assign (d0d2);
-
-    //Ipv4InterfaceContainer interfaces = address.Assign (devices);
-    Ipv4InterfaceContainer i0i1 = address.Assign (d0d1);
-    Ipv4InterfaceContainer i0i2 = address.Assign (d0d2);
-
-    VideoStreamClientHelper videoClient1 (i0i1.GetAddress (0), 5000);
-    ApplicationContainer clientApp1 = videoClient1.Install (nodes.Get (1));
-    clientApp1.Start (Seconds (1.0));
-    clientApp1.Stop (Seconds (100.0));
-
-    VideoStreamClientHelper videoClient2 (i0i2.GetAddress (0), 5000);
-    ApplicationContainer clientApp2 = videoClient2.Install (nodes.Get (2));
-    clientApp2.Start (Seconds (0.5));
-    clientApp2.Stop (Seconds (100.0));
-
-    VideoStreamServerHelper videoServer (5000);
-    videoServer.SetAttribute ("MaxPacketSize", UintegerValue (1400));
-    videoServer.SetAttribute ("FrameFile", StringValue ("./scratch/videoStreamer/small.txt"));
-    // videoServer.SetAttribute ("FrameSize", UintegerValue (4096));
-
-    ApplicationContainer serverApp = videoServer.Install (nodes.Get (0));
-    serverApp.Start (Seconds (0.0));
-    serverApp.Stop (Seconds (100.0));
-
-    pointToPoint.EnablePcap ("videoStream", d0d1.Get (1), false);
-    pointToPoint.EnablePcap ("videoStream", d0d2.Get (1), false);
-    Simulator::Run ();
-    Simulator::Destroy ();
-  }
-  else if (CASE == 3)
-  {
-    const uint32_t nWifi = 3, nAp = 1;
-    NodeContainer wifiStaNodes;
-    wifiStaNodes.Create (nWifi);  
-    NodeContainer wifiApNode;
-    wifiApNode.Create(nAp);   
-    
-    YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();   
-    YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();  
-    phy.SetChannel (channel.Create ());  
   
-    WifiHelper wifi;
-    wifi.SetRemoteStationManager ("ns3::AarfWifiManager");  
-  
-  
-    WifiMacHelper mac; 
-    Ssid ssid = Ssid ("ns-3-aqiao");  
-    mac.SetType ("ns3::StaWifiMac",    
-                "Ssid", SsidValue (ssid),   
-                "ActiveProbing", BooleanValue (false));  
-  
-    NetDeviceContainer staDevices;
-    staDevices = wifi.Install (phy, mac, wifiStaNodes);  
-  
-    mac.SetType ("ns3::ApWifiMac",   
-                "Ssid", SsidValue (ssid));   
-  
-    NetDeviceContainer apDevices;
-    apDevices = wifi.Install (phy, mac, wifiApNode);   
-
-    MobilityHelper mobility; 
-    mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
-                                "MinX", DoubleValue (0.0),
-                                "MinY", DoubleValue (0.0),
-                                "DeltaX", DoubleValue (30.0),
-                                "DeltaY", DoubleValue (30.0),
-                                "GridWidth", UintegerValue (2),
-                                "LayoutType", StringValue ("RowFirst"));
-
-    
-    mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel", "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));   
-    mobility.Install (wifiStaNodes);
-  
-    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");  
-    mobility.Install (wifiApNode);
-  
-    InternetStackHelper stack;
-    stack.Install (wifiApNode);
-    stack.Install (wifiStaNodes);   
-  
-    Ipv4AddressHelper address;
-  
-    address.SetBase ("10.1.3.0", "255.255.255.0");
-    
-    Ipv4InterfaceContainer apInterfaces;
-    apInterfaces = address.Assign (apDevices); 
-    Ipv4InterfaceContainer wifiInterfaces;
-    wifiInterfaces=address.Assign (staDevices);
-                  
-    //UdpEchoServerHelper echoServer (9);
-    VideoStreamServerHelper videoServer (5000);
-    videoServer.SetAttribute ("MaxPacketSize", UintegerValue (1400));
-    videoServer.SetAttribute ("FrameFile", StringValue ("./scratch/videoStreamer/small.txt"));
-    for(uint m=0; m<nAp; m++)
-    {
-      ApplicationContainer serverApps = videoServer.Install (wifiApNode.Get (m));
-      serverApps.Start (Seconds (0.0));
-      serverApps.Stop (Seconds (100.0));
-    }
-  
-    for(uint k=0; k<nWifi; k++)
-    {
-      VideoStreamClientHelper videoClient (apInterfaces.GetAddress (0), 5000);
-      ApplicationContainer clientApps =
-      videoClient.Install (wifiStaNodes.Get (k));
-      clientApps.Start (Seconds (0.5));
-      clientApps.Stop (Seconds (100.0));
-    }
-  
-    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-  
-    Simulator::Stop (Seconds (10.0));
-  
-    phy.EnablePcap ("wifi-videoStream", apDevices.Get (0));
-    AnimationInterface anim("wifi-1-3.xml");
-    Simulator::Run ();
-    Simulator::Destroy ();
-  }
-  else if (CASE == 4)
-  {
-    const uint32_t nWifi = 3, nAp = 3;
-    NodeContainer wifiStaNodes;
-    wifiStaNodes.Create (nWifi);  
-    NodeContainer wifiApNode;
-    wifiApNode.Create(nAp);   
-    
-    YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();   
-    YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();  
-    phy.SetChannel (channel.Create ());  
-  
-    WifiHelper wifi;
-    wifi.SetRemoteStationManager ("ns3::AarfWifiManager");  
-  
-  
-    WifiMacHelper mac; 
-    Ssid ssid = Ssid ("ns-3-aqiao");  
-    mac.SetType ("ns3::StaWifiMac",    
-                "Ssid", SsidValue (ssid),   
-                "ActiveProbing", BooleanValue (false));  
-  
-    NetDeviceContainer staDevices;
-    staDevices = wifi.Install (phy, mac, wifiStaNodes);  
-  
-    mac.SetType ("ns3::ApWifiMac",   
-                "Ssid", SsidValue (ssid));   
-  
-    NetDeviceContainer apDevices;
-    apDevices = wifi.Install (phy, mac, wifiApNode);   
-    MobilityHelper mobility; 
-    mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
-                                  "MinX", DoubleValue (0.0),
-                                  "MinY", DoubleValue (0.0),
-                                  "DeltaX", DoubleValue (50.0),
-                                  "DeltaY", DoubleValue (30.0),
-                                  "GridWidth", UintegerValue (3),
-                                  "LayoutType", StringValue ("RowFirst"));
-  
-    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");  
-    mobility.Install (wifiApNode);
-      
-    //mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",   
-    //                           "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));   
-    //mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");  
-    mobility.Install (wifiStaNodes);
-  
-    InternetStackHelper stack;
-    stack.Install (wifiApNode);
-    stack.Install (wifiStaNodes);   
-  
-    Ipv4AddressHelper address;
-  
-    address.SetBase ("10.1.3.0", "255.255.255.0");
-    
-    Ipv4InterfaceContainer apInterfaces;
-    apInterfaces = address.Assign (apDevices); 
-    Ipv4InterfaceContainer wifiInterfaces;
-    wifiInterfaces=address.Assign (staDevices);
-                  
-    //UdpEchoServerHelper echoServer (9);
-    VideoStreamServerHelper videoServer (5000);
-    videoServer.SetAttribute ("MaxPacketSize", UintegerValue (1400));
-    videoServer.SetAttribute ("FrameFile", StringValue ("./scratch/videoStreamer/small.txt"));
-    for(uint m=0; m<nAp; m++)
-    {
-      ApplicationContainer serverApps = videoServer.Install (wifiApNode.Get (m));
-      serverApps.Start (Seconds (0.0));
-      serverApps.Stop (Seconds (100.0));
-    }
-  
-    for(uint k=0; k<nWifi; k++)
-    {
-      VideoStreamClientHelper videoClient (apInterfaces.GetAddress (k), 5000);
-      ApplicationContainer clientApps =
-      videoClient.Install (wifiStaNodes.Get (k));
-      clientApps.Start (Seconds (0.5));
-      clientApps.Stop (Seconds (100.0));
-    }
-  
-    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-  
-    Simulator::Stop (Seconds (10.0));
-  
-    phy.EnablePcap ("wifi-videoStream", apDevices.Get (0));
-    AnimationInterface anim("wifi-1-3.xml");
-    Simulator::Run ();
-    Simulator::Destroy ();
-  }
-  else if (CASE == 5)
-  {
-    const uint32_t nWifi = nodeNum + 2, nAp = nodeNum + 2;
-    NodeContainer wifiStaNodes;
-    wifiStaNodes.Create (nWifi);
-    NodeContainer wifiApNode;
-    wifiApNode.Create(nAp);
-    
-    YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
-    YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
-    phy.SetChannel (channel.Create ());
-  
-    WifiHelper wifi;
-    wifi.SetRemoteStationManager ("ns3::AarfWifiManager");
-  
-    WifiMacHelper mac; 
-    Ssid ssid = Ssid ("ns-3-aqiao");  
-    mac.SetType ("ns3::StaWifiMac",    
-                "Ssid", SsidValue (ssid),   
-                "ActiveProbing", BooleanValue (false));  
-  
-    NetDeviceContainer staDevices;
-    staDevices = wifi.Install (phy, mac, wifiStaNodes);  
-  
-    mac.SetType ("ns3::ApWifiMac",   
-                "Ssid", SsidValue (ssid));   
-  
-    NetDeviceContainer apDevices;
-    apDevices = wifi.Install (phy, mac, wifiApNode);   
-    MobilityHelper mobility; 
-    mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
-                                  "MinX", DoubleValue (0.0),
-                                  "MinY", DoubleValue (0.0),
-                                  "DeltaX", DoubleValue (50.0),
-                                  "DeltaY", DoubleValue (30.0),
-                                  "GridWidth", UintegerValue (3),
-                                  "LayoutType", StringValue ("RowFirst"));
-  
-    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");  
-    mobility.Install (wifiApNode);
-      
-    //mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",   
-    //                           "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));   
-    //mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");  
-    mobility.Install (wifiStaNodes);
-  
-    InternetStackHelper stack;
-    stack.Install (wifiApNode);
-    stack.Install (wifiStaNodes);   
-  
-    Ipv4AddressHelper address;
-  
-    address.SetBase ("10.1.3.0", "255.255.255.0");
-    
-    Ipv4InterfaceContainer apInterfaces;
-    apInterfaces = address.Assign (apDevices); 
-    Ipv4InterfaceContainer wifiInterfaces;
-    wifiInterfaces=address.Assign (staDevices);
-                  
-    //UdpEchoServerHelper echoServer (9);
-    VideoStreamServerHelper videoServer (5000);
-    videoServer.SetAttribute ("MaxPacketSize", UintegerValue (1400));
-    videoServer.SetAttribute ("FrameFile", StringValue ("./scratch/videoStreamer/small.txt"));
-    for(uint m=0; m<nAp; m++)
-    {
-      ApplicationContainer serverApps = videoServer.Install (wifiApNode.Get (m));
-      serverApps.Start (Seconds (0.0));
-      serverApps.Stop (Seconds (100.0));
-    }
-  
-    for(uint k=0; k<bridgeNum+2; k++)
-    {
-      VideoStreamClientHelper videoClient (apInterfaces.GetAddress (route[k][1]), 5000);
-      ApplicationContainer clientApps =
-      videoClient.Install (wifiStaNodes.Get (route[k][0]));
-      clientApps.Start (Seconds (0.5));
-      clientApps.Stop (Seconds (100.0));
-    }
-  
-    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-  
-    Simulator::Stop (Seconds (10.0));
-  
-    phy.EnablePcap ("wifi-videoStream", apDevices.Get (nWifi - 1));
-    AnimationInterface anim("wifi-1-3.xml");
-    Simulator::Run ();
-    Simulator::Destroy ();
-  }
-  else if (CASE == 6) {
+  if (CASE == 6) {
     NodeContainer nodes;
     nodes.Create (nodeNum + 2);
-
+    std::string delaytime;
     bridgeNum += 2;
-    std::vector<NodeContainer> linkvector;
+
+    std::vector<NodeContainer> linkvector(bridgeNum);
     for(uint i=0; i<bridgeNum; i++){
-        linkvector[i] = NodeContainer(nodes.Get(route[i][0]),nodes.Get(route[i][1]))
+        linkvector[i] = NodeContainer(nodes.Get(route[i][0]),nodes.Get(route[i][1]));
     }
 
-    std::vector<PointToPointHelper> p2pvector;
-    for(uint i=0; i<bridgeNum; i++){
-        p2pvector[i].SetChannelAttribute("DataRate", StringValue("1Mbps"));
-        delaytime = string(route[i][2])+"ms";
-        p2pvector[i].SetChannelAttribute("Delay", StringValue(delaytime));
-    }
+    //std::vector<PointToPointHelper> p2pvector(bridgeNum);
+    //for(uint i=0; i<bridgeNum; i++){
+    //    p2pvector[i].SetDeviceAttribute("DataRate", StringValue("1Mbps"));
+    //    delaytime = std::to_string(route[i][2])+"ms";
+    //    p2pvector[i].SetChannelAttribute("Delay", StringValue(delaytime));
+    //}
+    
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("2Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
 
-    std::vector<NetDeviceContainer> netvector;
+    std::vector<NetDeviceContainer> netvector(bridgeNum);
     for(uint i=0; i<bridgeNum; i++){
-        netvector[i] = p2pvector[i].Install(linkvector[i]);
+        netvector[i] = pointToPoint.Install(linkvector[i]);
     }
 
     InternetStackHelper stack;
     stack.Install (nodes);
 
     Ipv4AddressHelper address;
-    string address_value = "10.1.1.0";
+    std::string address_value = "10.1.1.0";
     for(uint i=0; i<bridgeNum; i++){
-        address.SetBase(address_value, "255.255.255.0");
+        int num = route[i][1];
+        address_value = "10.1."+std::to_string(num)+".0";
+        address.SetBase(Ipv4Address(address_value.c_str()), "255.255.255.0");
         address.Assign(netvector[i]);
-        string temp = address_value.substr(3);
-        int pos = temp.find('.');
-        int num = int(temp.substr(0,ps))+1;
-        address_value = "10."+string(num)+".1.0";
     }
 
-    std::vector<Ipv4InterfaceContainer> interfacevector;
+    std::vector<Ipv4InterfaceContainer> interfacevector(bridgeNum);
     for(uint i=0; i<bridgeNum; i++){
         interfacevector[i] = address.Assign(netvector[i]);
     }
-
-    VideoStreamClientHelper videoClient1 (interfacevector[0].GetAddress (0), 5000);
-    ApplicationContainer clientApp1 = videoClient1.Install (nodes.Get (0));
-    clientApp1.Start (Seconds (1.0));
-    clientApp1.Stop (Seconds (100.0));
+    
+    for(uint k=0; k<bridgeNum; k++){
+        VideoStreamClientHelper videoClient (interfacevector[k].GetAddress (0), 5000);
+        ApplicationContainer clientApps = videoClient.Install (nodes.Get (route[k][0]));
+        clientApps.Start (Seconds (0.0));
+        clientApps.Stop (Seconds (100.0));
+    }
 
     VideoStreamServerHelper videoServer (5000);
     videoServer.SetAttribute ("MaxPacketSize", UintegerValue (1400));
     videoServer.SetAttribute ("FrameFile", StringValue ("./scratch/videoStreamer/small.txt"));
     // videoServer.SetAttribute ("FrameSize", UintegerValue (4096));
-
-    ApplicationContainer serverApp1 = videoServer.Install (nodes.Get(nodeNum+1));
-    serverApp1.Start (Seconds (0.0));
-    serverApp1.Stop (Seconds (100.0));
-
-    pointToPoint.EnablePcap ("videoStream", netvector[0].Get (1), false);
+    for(uint k=0; k<nodeNum+2; k++){
+        ApplicationContainer serverApps = videoServer.Install(nodes.Get(k));
+        serverApps.Start(Seconds(0.0));
+        serverApps.Stop(Seconds(100.0));
+    }
+    
+    pointToPoint.EnablePcap("videoStream", netvector[0].Get(0),false);
+    
+    
     Simulator::Run ();
     Simulator::Destroy ();
   }
